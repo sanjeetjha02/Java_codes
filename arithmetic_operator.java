1f748765b2ac6228749d8e6086cc2af4da1bc05b
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-10 19:27:31 +0530

    arithematic operator

---
+++ arithmetic_operator.java
@@ -0,0 +1,36 @@
+public class arithmetic_operator {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 6;
+        System.out.println(a+b);
+        System.out.println(a%2); //modulo gives remainder
+        System.out.println(a/b); //division
+        System.out.println(a*b);
+        System.out.println(++a);
+        System.out.println(++a + b);
+        System.out.println();
+        String c = "hello";
+        String d  = " chutiya";
+        System.out.println(c+d);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println("ASSIGNMENT OPERATOR");
+        System.out.println(9==9);
+        System.out.println(a+=b);
+        System.out.println("in and & operator all conditions should be true");
+//in and & operator all conditions should be true    
+        System.out.println(64>5 && 64>100);
+        System.out.println(64>5 && 64<100);
+        System.out.println(" in or operator any one condition should be true ");
+// in or operator any one condition should be true 
+        System.out.println(64>5 || 64>100);
+        System.out.println(64<5 || 64>100);
+        System.out.println("// not operator");
+// not operator 
+        System.out.println(8!=10);
+        System.out.println(8!=8);
+
+    }
+    
+}
+
