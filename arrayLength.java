2d5a6b4f5fdccf524881fff787c5ed5ca6a4acd8
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-13 00:33:05 +0530

    Add files via upload

---
+++ arrayLength.java
@@ -0,0 +1,10 @@
+public class arrayLength {
+    public static void main(String[] args) {
+        int [] marks = {93,34,76,23,80};
+        System.out.println(marks.length);
+        System.out.println(marks[0]);
+        
+
+    }
+    
+}
