8be32edae6f7ce15875d4484146388717140c1ac
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-13 00:33:41 +0530

    Add files via upload

---
+++ boolint.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class boolint {
+    public static void main(String[] args) {
+        System.out.println("checkin whether a number is int of not : ");
+        Scanner sc = new Scanner(System.in);
+        boolean b1 = sc.hasNextInt();
+        System.out.println(b1);
+
+        
+    }
+    
+}
