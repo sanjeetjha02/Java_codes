3f9968e08175923111dfeffa1d20f7ca899afa18
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-21 22:43:29 +0530

    Add files via upload

---
+++ escape_sequence.java
@@ -0,0 +1,7 @@
+public class escape_sequence {
+    public static void main(String[] args) {
+        String escape1 = " dear sanjeet, \n\tReading java is fun. \n thanks!";
+        System.out.println(escape1);
+    }
+    
+}
