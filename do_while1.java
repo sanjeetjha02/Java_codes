014f6bb38ccb08f6b07a5a42b12c712b45aa028a
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-21 22:42:13 +0530

    Add files via upload

---
+++ do_while1.java
@@ -0,0 +1,10 @@
+public class do_while1 {
+    public static void main(String[] args) {
+        int b = 0;
+        do{
+            System.out.println(b);
+            b++;
+        }while(b<=5);
+    }
+    
+}
