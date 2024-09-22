5aa9c04495741dc50b9bcc27d8c554ad5aa58c55
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-22 21:43:29 +0530

    Add files via upload

---
+++ good_day.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class good_day {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what is your name : ");
+        String name = sc.next();
+        System.out.println(" hello "+ name + " how are you ");
+    }
+
+}
