8f4c598c046a57a8dc163170111d417e1ce6db0d
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-22 21:42:53 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class for_loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter stsrting point");
+        int a = sc.nextInt();
+        System.out.println("enter ending point");
+        int b = sc.nextInt();
+        for(int i=a; i<=b; i++){
+            System.out.println(2*i+1);
+        }
+
+        
+    }
+    
+}
