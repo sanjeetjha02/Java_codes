95f5ff158140453a43414a81fea2fa4371682e0b
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-09-29 21:53:20 +0530

    Add files via upload

---
+++ print_pattern.java
@@ -0,0 +1,15 @@
+public class print_pattern {
+    public static void main(String[] args) {
+        //practice prooblem 1 ch 5
+        int n=10;
+        for(int i=n; i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+
+            }
+            System.out.print("\n");
+        }
+        
+    }
+    
+}
