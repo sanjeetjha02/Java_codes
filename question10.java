7c09039be4991c26294da0c284ec9c93a20c0a50
Sanjeet Kumar <158173744+sanjeetjha02@users.noreply.github.com>
2024-10-28 15:26:04 +0530

    Add files via upload
     Write a Java program that takes a year from the user and prints whether it is a leap year or not.

---
+++ question10.java
@@ -0,0 +1,25 @@
+/*
+ Write a Java program that takes a year from the user and prints whether it is a leap year or not.
+ */
+package conditional_statements;
+
+import java.util.Scanner;
+public class question10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("input the year: ");
+        int year = sc.nextInt();
+
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+        if (x && (y || z)) {
+            System.out.println( year + " is a leap year");
+        }
+        else {
+            System.out.println(year + "is not a leap year");
+        }
+    }
+}
